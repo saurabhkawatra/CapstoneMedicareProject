3d83e01dfa37415e0568433496b01e426fd1a842
saurabhkawatra <saurabh.kawatra1@gmail.com>
2021-10-05 14:39:30 +0530

    first commit

---
+++ src/main/java/com/Medicare/CapstoneMedicareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Medicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneMedicareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneMedicareProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Medicare/CapstoneMedicareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneMedicareProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
